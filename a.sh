472943b2b48c6e326e349ed83958cf2db2a17e30
Sabri-zaki <57401357+fateh32-sod0@users.noreply.github.com>
2019-11-24 13:19:44 +0100

    Add files via upload

---
+++ a.sh
@@ -0,0 +1,86 @@
+#!/bin/bash
+echo $red 
+ echo ""####################### ""
+echo " This Script is written science DIY channel. "
+echo"##############################"
+
+echo "007"
+echo "007"
+echo "007"
+echo "."
+
+echo  "#############################"
+echo   "  SUBSCRIBE    scince DIY"
+ echo   "          AK         "                
+echo "     
+   "#############################
+
+echo "007"
+echo "007"
+echo "007"
+
+echo "########################"
+
+apt update -y                      #update in progress
+apt install python -y           #installation......
+apt install python2 -y
+apt install ruby -y 
+apt install git -y
+
+echo  "###########################"
+echo   "  SUBSCRIBE  science DIY "
+ echo   "   AK "                                                                    
+echo   "####################################"
+
+apt install php -y 
+apt install perl -y
+apt install nmap -y
+
+echo  "#####################################"
+echo   "  SUBSCRIBE    Science DIY " 
+ echo   "   AK"                                                                    
+echo   "####################################"
+
+apt install bash -y
+apt install clang -y
+apt install jq -y
+apt install macchanger -y
+
+echo  "#####################################"
+echo   "  SUBSCRIBE    science DIY "
+ echo   "   AK"                                                                    
+echo   "####################################"
+
+apt install nano -y
+apt install curl -y
+apt install tar -y
+apt install zip -y
+
+echo  "#####################################"
+echo   "  SUBSCRIBE    scince DIY "
+    echo   "  AK"                                                              
+echo   "####################################"
+
+apt install unzip -y
+apt install tor -y 
+apt install sudo -y
+apt install wget -y
+
+echo  "#####################################"
+echo   "  SUBSCRIBE    science DIY"
+ echo   "  AK "                                                                    
+echo   "####################################"
+
+apt install wcalc -y 
+apt install openssl
+apt install bmon -y
+
+echo  "#####################################"
+echo   "  SUBSCRIBE    science DIY "
+ echo   "   AK"                                                                    
+echo   "####################################"
+
+echo " If You Connected To Internet Then  "
+echo " YOUR PACKAGES IS INSTALLED SUCCESSFULLY "
+
+exit
